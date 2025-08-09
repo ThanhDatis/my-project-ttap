e84c872dddf48b38a345ad8ea3b58c336b656618
Tadd <2251120340@ut.edu.vn>
2025-08-09 15:12:02 +0700

    Add code formatting rules and initial shared components

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,47 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-runtime': 'off',
+      'no-unused-vars': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'lf',
+          semi: true,
+          singleQuote: false,
+          tabWidth: 2,
+          printWidth: 120,
+          plugin: ['@ianvs/prettier-plugin-sort-imports'],
+        }
+      ]
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
