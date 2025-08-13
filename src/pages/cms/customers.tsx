1b0bf20373efa71cc7ce367d51553a63a4e6ea96
Tadd <2251120340@ut.edu.vn>
2025-08-13 17:18:11 +0700

    move category page files into cms directory

---
+++ src/pages/cms/customers.tsx
@@ -0,0 +1,17 @@
+import { Typography, Box } from '@mui/material';
+
+const Customers = () => {
+  return (
+    <Box>
+      <Typography variant="h4" sx={{ fontWeight: 'bold', mb: 2 }}>
+        THIS IS CUSTOMERS
+      </Typography>
+      <Typography variant="body1" color="text.secondary">
+        Manage your customers here. You can view customer information,
+        add new customers, edit existing records, and track customer activities.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Customers;
